88a2bb055ca96c2ff33d9aa13b2f6a5b937f4968
Chaley Boatmon <chaleyboatmon@gmail.com>
2022-12-08 18:51:49 -0600

    Improve readability for catYears js file

--- JS/catYears.js
+++ JS/catYears.js
@@ -1,28 +1,25 @@
-// My actual human age is stored in 'myAge'
-let myAge = 25;
+// My actual human age is stored in 'myHumanAge'
+const myHumanAge = 25;
 
-// A value of 2 is set to 'earlyYears' to represent the first 2 human years
-let earlyYears = 2;
+// A value of 2 is set to 'earlyHumanYears'
+const earlyHumanYears = 2;
 
-// 'earlyYears' is multipled by 25 to convert human years into early cat years
-// This value is reassigned to 'earlyYears'
-earlyYears *= 25;
+// 'earlyHumanYears' is multipled by 25 and converted into 'earlyCatYears'
+const earlyCatYears = earlyHumanYears * 25;
 
-// 'myAge' - 2 years leaves the remaining years which will be stored in 'laterYears'
-let laterYears = myAge - 2;
+// 'myAge' - 'earlyHumanYears' leaves the 'laterHumanYears'
+const laterHumanYears = myHumanAge - earlyHumanYears;
 
+// 'laterHumanYears' is multiplied by 4 and converted into 'laterCatYears'
+const laterCatYears = laterHumanYears * 4;
 
-// 'laterYears' is multiplied by 4 to convert the human years into later cat years
-// This value is reassigned to 'laterYears'
-laterYears *= 4;
-
-// 'earlyYears' and 'laterYears' are added together to get 'myAgeInCatYears'
-const myAgeInCatYears = earlyYears + laterYears;
+// 'earlyCatYears' and 'laterCatYears' are added together to get 'myAgeInCatYears'
+const myAgeInCatYears = earlyCatYears + laterCatYears;
 
 // My name is stored in 'myName'
 const myName = 'Chaley';
 
 // The 'myCatAgeMessage' will read 'Hi! My name is NAME. I am HUMAN AGE years old in human years, which is CAT AGE years old in cat years.'
-const myCatAgeMessage = 'Hi! My name is ' + myName + '. I am ' + myAge + ' years old in human years, which is ' + myAgeInCatYears + ' years old in cat years.';
+const myCatAgeMessage = 'Hi! My name is ' + myName + '. I am ' + myHumanAge + ' years old in human years, which is ' + myAgeInCatYears + ' years old in cat years.';
 
 console.log(myCatAgeMessage);
