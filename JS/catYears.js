d2e527a28ff4850e8f32e6025eff2124d279c647
Chaley Boatmon <chaleyboatmon@gmail.com>
2022-12-10 15:08:21 -0600

    Second readability improvement for catYears js file

--- JS/catYears.js
+++ JS/catYears.js
@@ -1,25 +1,13 @@
-// My actual human age is stored in 'myHumanAge'
-const myHumanAge = 25;
-
-// A value of 2 is set to 'earlyHumanYears'
 const earlyHumanYears = 2;
+const oneEarlyCatYear = 25;
+const totalEarlyCatYears = oneEarlyCatYear * earlyHumanYears;
 
-// 'earlyHumanYears' is multipled by 25 and converted into 'earlyCatYears'
-const earlyCatYears = earlyHumanYears * 25;
-
-// 'myAge' - 'earlyHumanYears' leaves the 'laterHumanYears'
+const myHumanAge = 25;
 const laterHumanYears = myHumanAge - earlyHumanYears;
 
-// 'laterHumanYears' is multiplied by 4 and converted into 'laterCatYears'
-const laterCatYears = laterHumanYears * 4;
+const oneLaterCatYear = 4;
+const totalLaterCatYears = oneLaterCatYear * laterHumanYears;
+const myAgeInCatYears = totalEarlyCatYears + totalLaterCatYears;
 
-// 'earlyCatYears' and 'laterCatYears' are added together to get 'myAgeInCatYears'
-const myAgeInCatYears = earlyCatYears + laterCatYears;
-
-// My name is stored in 'myName'
 const myName = 'Chaley';
-
-// The 'myCatAgeMessage' will read 'Hi! My name is NAME. I am HUMAN AGE years old in human years, which is CAT AGE years old in cat years.'
-const myCatAgeMessage = 'Hi! My name is ' + myName + '. I am ' + myHumanAge + ' years old in human years, which is ' + myAgeInCatYears + ' years old in cat years.';
-
-console.log(myCatAgeMessage);
+console.log('Hi! My name is ' + myName + '. I am ' + myHumanAge + ' years old in human years, which is ' + myAgeInCatYears + ' years old in cat years.');
