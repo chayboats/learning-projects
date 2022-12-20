b3f23e7347f6ea12fdc8cc6e0cd49135ea61c454
Chaley Boatmon <chaleyboatmon@gmail.com>
2022-12-20 14:01:59 -0600

    Fix parameters for code mathPractice.js

--- JS/mathPractice.js
+++ JS/mathPractice.js
@@ -11,8 +11,8 @@
 // So all of the ingredients for her recipe will have to be converted to tbsp
 
 // 1 cup = 16tbsp
-const cupsToTablespoons = (a) => {
-  return a * 16;
+const cupsToTablespoons = ( cups, tablespoons = 16 ) => {
+  return cups * tablespoons;
 }
 
 let flourTablespoons = cupsToTablespoons(2.5);
@@ -21,8 +21,8 @@ let buttermilkTablespoons = cupsToTablespoons(1) + 3;
 
 
 // 1 tsp = 1/3 tbsp
-const teaspoonToTablespoon = (a) => {
-  return a * (1/3)
+const teaspoonToTablespoon = ( teaspoons, tablespoons = 1/3 ) => {
+  return teaspoons * tablespoons
 }
 
 let saltTablespoons = teaspoonToTablespoon(1);
@@ -31,16 +31,17 @@ let honeyTablespoons = teaspoonToTablespoon(2);
 
 // Jill wants to make 200 biscuits for her restaurant. 
 // She will need 25 times the original ingredients
-const servingMultiplier = (a) => {
-  return a * 25
+
+const servingMultiplier = ( serving, multiplier = 25 ) => {
+  return serving * multiplier
 }
 
-flourTablespoons = servingMultiplier(flourTablespoons).toFixed(1);
-bakingPowderTablespoons = servingMultiplier(2).toFixed(1);
-saltTablespoons = servingMultiplier(saltTablespoons).toFixed(1);
-honeyTablespoons = servingMultiplier(honeyTablespoons).toFixed(1);
-butterTablespoons = servingMultiplier(butterTablespoons.toFixed(1));
-buttermilkTablespoons = servingMultiplier(buttermilkTablespoons).toFixed(1);
+flourTablespoons = servingMultiplier( flourTablespoons );
+bakingPowderTablespoons = servingMultiplier(2);
+saltTablespoons = servingMultiplier( saltTablespoons );
+honeyTablespoons = servingMultiplier( honeyTablespoons );
+butterTablespoons = servingMultiplier( butterTablespoons );
+buttermilkTablespoons = servingMultiplier( buttermilkTablespoons );
 
 
 // Jill is shopping at Walmart
@@ -48,57 +49,76 @@ buttermilkTablespoons = servingMultiplier(buttermilkTablespoons).toFixed(1);
 // Jill will need to convert the ingredients to grams
 
 // 1tbsp = 14.3grams
-const tablespoonsToGrams = (a) => {
-  return a * 14.3
+// TODO: fix parameters
+const tablespoonsToGrams = ( tablespoons, grams = 14.3 ) => {
+  return tablespoons * grams
 }
 
-const flourGrams = tablespoonsToGrams(flourTablespoons);
-const bakingPowderGrams = tablespoonsToGrams(bakingPowderTablespoons);
-const saltGrams = tablespoonsToGrams(saltTablespoons);
-const honeyGrams = tablespoonsToGrams(honeyTablespoons);
-const butterGrams = tablespoonsToGrams(butterTablespoons);
-const buttermilkGrams = tablespoonsToGrams(buttermilkTablespoons);
+const flourGrams = tablespoonsToGrams( flourTablespoons );
+const bakingPowderGrams = tablespoonsToGrams( bakingPowderTablespoons );
+const saltGrams = tablespoonsToGrams( saltTablespoons );
+const honeyGrams = tablespoonsToGrams( honeyTablespoons );
+const butterGrams = tablespoonsToGrams( butterTablespoons );
+const buttermilkGrams = tablespoonsToGrams( buttermilkTablespoons );
 
 
 
-// Jill needs to figure out how many containers of each ingredient she will need
+// Jill needs to figure out how many packages of each ingredient she will need
 
 // Walmart Groceries and Prices
-// 2300g flour per container = $3
-// 230g baking powder per container = $2
-// 740g salt per container= $0.50
-// 340g honey per container = $4
-// 460g butter per container = $5
-// 970g buttermilk per container = $1.50
+// 2300g flour per package = $3
+// 230g baking powder per package = $2
+// 740g salt per package= $0.50
+// 340g honey per package = $4
+// 460g butter per package = $5
+// 970g buttermilk per package = $1.50
 
-// Grams needed / Grams per container = Containers Needed
-const containersNeeded = ( a, b ) => {
-  return a / b 
+// Grams needed / Grams per package = Packages Needed
+const packagesNeeded = ( gramsNeeded, gramsPerPackage ) => {
+  return gramsNeeded / gramsPerPackage 
 }
 
-const flourContainers = Math.ceil(containersNeeded( flourGrams, 2300 ).toFixed(1));
-const bakingPowderContainers = Math.ceil(containersNeeded( bakingPowderGrams, 230 ).toFixed(1));
-const saltContainers = Math.ceil(containersNeeded( saltGrams, 740 ).toFixed(1));
-const honeyContainers = Math.ceil(containersNeeded( honeyGrams, 340 ).toFixed(1));
-const butterContainers = Math.ceil(containersNeeded( butterGrams, 460 ).toFixed(1));
-const buttermilkContainers = Math.ceil(containersNeeded( buttermilkGrams, 970 ).toFixed(1));
+let flourPackages = packagesNeeded( flourGrams, 2300 );
+let bakingPowderPackages = packagesNeeded( bakingPowderGrams, 230 );
+let saltPackages = packagesNeeded( saltGrams, 740 );
+let honeyPackages = packagesNeeded( honeyGrams, 340 );
+let butterPackages = packagesNeeded( butterGrams, 460 );
+let buttermilkPackages = packagesNeeded( buttermilkGrams, 970 );
+
+// If Jill needs a little more than what's in one package of ingredients, 
+// she will have to round up and get another full package
+
+flourPackages = Math.ceil( flourPackages.toFixed(1) );
+bakingPowderPackages = Math.ceil( bakingPowderPackages.toFixed(1) );
+saltPackages = Math.ceil( saltPackages.toFixed(1) );
+honeyPackages = Math.ceil( honeyPackages.toFixed(1) );
+butterPackages = Math.ceil( butterPackages.toFixed(1) );
+buttermilkPackages = Math.ceil( buttermilkPackages.toFixed(1) );
+
 
 // Jill needs to figure out the cost for each ingredient
 
-// Number of containers for ingredient * Price for Container = Cost for Ingredient
-const unitPriceMultipler = ( a, b ) => {
-  return a * b
+// Number of packages for ingredient * Price for package = Cost for Ingredient
+const unitPriceMultipler = ( packageNumber, packagePrice ) => {
+  return packageNumber * packagePrice
 }
 
-const flourCost = unitPriceMultipler( flourContainers, 3);
-const bakingPowderCost = unitPriceMultipler(bakingPowderContainers, 2 );
-const saltCost = unitPriceMultipler( saltContainers, .5 );
-const honeyCost = unitPriceMultipler( honeyContainers, 4 );
-const butterCost = unitPriceMultipler( butterContainers, 5 );
-const buttermilkCost = unitPriceMultipler( buttermilkContainers, 1.5 );
+const flourCost = unitPriceMultipler( flourPackages, 3);
+const bakingPowderCost = unitPriceMultipler(bakingPowderPackages, 2 );
+const saltCost = unitPriceMultipler( saltPackages, .5 );
+const honeyCost = unitPriceMultipler( honeyPackages, 4 );
+const butterCost = unitPriceMultipler( butterPackages, 5 );
+const buttermilkCost = unitPriceMultipler( buttermilkPackages, 1.5 );
 
 // Jill needs to figure out the total
-const totalCost = flourCost + bakingPowderCost + saltCost + honeyCost + butterCost + buttermilkCost
+const totalCost = 
+  flourCost + 
+  bakingPowderCost + 
+  saltCost + 
+  honeyCost + 
+  butterCost + 
+  buttermilkCost
+;
 
 // Jill will be paying with a hundred dollar bill. 
 // She needs to know how much change she will get back.
@@ -107,15 +127,28 @@ const change = 100 - totalCost;
 
 
 // Jill will need a new list for her recipe as well as a shopping list
-function List(flour, bakingPowder, salt, honey, butter, buttermilk)  {
-  this.flour = flour;
-  this.bakingPowder = bakingPowder;
-  this.salt = salt;
-  this.honey = honey;
-  this.butter = butter;
-  this.buttermilk = buttermilk;
+
+const List = ( flour, bakingPowder, salt, honey, butter, buttermilk ) => {
+  return { flour, bakingPowder, salt, honey, butter, buttermilk }
 }
-const recipe = new List( flourTablespoons + ' tablesooons', bakingPowderTablespoons + ' tablespoons', saltTablespoons + ' tablespoons', honeyTablespoons + ' tablespoons', butterTablespoons + ' tablespoons', buttermilkTablespoons + ' tablespoons' )
-const shoppingList = new List ( flourContainers + ' containers', bakingPowderContainers + ' containers', saltContainers + ' containers', honeyContainers + ' containers', butterContainers + ' containers', butterContainers + ' containers' );
+
+const recipe = List(
+  flourTablespoons + ' tablespoons', 
+  bakingPowderTablespoons + ' tablespoons', 
+  saltTablespoons.toFixed(1) + ' tablespoons', 
+  honeyTablespoons.toFixed(1) + ' tablespoons', 
+  butterTablespoons + ' tablespoons', 
+  buttermilkTablespoons + ' tablespoons' 
+)
+
+const shoppingList = List( 
+  flourPackages + ' packages', 
+  bakingPowderPackages + ' packages', 
+  saltPackages + ' package', 
+  honeyPackages + ' package', 
+  butterPackages + ' packages', 
+  butterPackages + ' packages' 
+);
 
 
+console.log(shoppingList);
