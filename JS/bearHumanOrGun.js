cced2c24793e883edb66548e198475ddfec74b24
Chaley Boatmon <chaleyboatmon@gmail.com>
2023-01-02 10:46:13 -0600

    Added second version of bear human or gun

--- JS/bearHumanOrGun.js
+++ JS/bearHumanOrGun.js
@@ -4,8 +4,8 @@ function getUserChoice(userInput) {
   if(userInput === 'bear' || userInput === 'human' || userInput === 'gun'){
     return userInput
   }
-  
-  return 'Please choose BEAR, HUMAN, or GUN.';
+
+  return 'Please choose bear, human, or gun.';
 };
 
 function getComputerChoice() {
@@ -16,22 +16,43 @@ function getComputerChoice() {
     } else if(randomNumber === 1) {
       return 'human'
     } 
-    return 'gun'
+
+  return 'gun'
 }
 
 function determineWinner(userChoice, computerChoice) {
-  if(userChoice===computerChoice) {
+  if (userChoice === computerChoice) {
     return 'It\'s a tie!'
-  } else if(
-    userChoice === 'bear' && computerChoice === 'human' 
-    || 
-    userChoice === 'human' && computerChoice === 'gun' 
-    || 
-    userChoice === 'gun' && computerChoice === 'bear'
-    ){
-    return 'You win!'
-    } 
-    return 'Computer wins!'
+  
+  } else if (userChoice === 'bear' && computerChoice === 'human') {
+    return 'You have mauled a human!'
+
+  } else if (userChoice === 'bear' && computerChoice === 'gun') {
+    return 'You were shot by a gun.'
+
+  } else if (userChoice === 'human' && computerChoice === 'bear') {
+    return 'You were mauled by a bear.'
+
+  } else if (userChoice === 'human' && computerChoice === 'gun') {
+    return 'You disarmed a gun!'
+
+  } else if (userChoice === 'gun' && computerChoice === 'bear') {
+    return 'You shot a bear!'
+
+  } else if (userChoice === 'gun' && computerChoice === 'human') {
+    return 'You were disarmed by a human.'
+  }
+  return 'Error'
 }
 
-console.log(determineWinner(getUserChoice('bear'), getComputerChoice))
+function playGame() {
+  const promptUserChoice = prompt('Please choose bear, human, or gun');
+  // Prompt is a feature in a browser, so it will not just work for VS Code
+
+  const userChoice = getUserChoice(promptUserChoice);
+  console.log('user: ' + userChoice);
+  const computerChoice = getComputerChoice();
+  console.log('computer: ' + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+playGame()
---
+++ JS/bearHumanOrGunTry2.js
@@ -0,0 +1,46 @@
+function getUserChoice(userInput) {
+  userInput = userInput.toLowerCase()
+  
+  if (userInput === 'bear' || userInput === 'human' || userInput === 'gun') {
+    return userInput
+  }
+
+  return 'Please choose BEAR, HUMAN, or GUN.'
+}
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(math.random() *  3)
+  
+  if(randomNumber === 0) {
+    return 'bear'
+  } else if(randomNumber === 1) {
+    return 'human'
+  }
+
+  return 'gun'
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if(userChoice === computerChoice) {
+    return "It's a tie!"
+  
+  } else if((userChoice === 'bear' && computerChoice === 'human')
+  || (userChoice === 'human' && computerChoice === 'gun')
+  || (userChoice === 'gun' && computerChoice === 'bear')
+  ){
+    return 'You win!'
+  }
+
+  return 'Computer wins!'
+}
+
+function playGame() {
+  const userPrompt = prompt('Please choose BEAR, HUMAN, or GUN.');
+  const userChoice = getUserChoice(userPrompt);
+  console.log('User: ' + userChoice);
+  const computerChoice = getComputerChoice();
+  console.log ('Computer: ' + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice))
+}
+
+playGame()
